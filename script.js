8ad898ffcbe979f5e3674b8dd104680c9490ee9f
Viraj Kanase <virajv2k@gmail.com>
2021-12-13 18:20:42 +0530

    Fix: Hide menu upon click for 600px

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('navbar__toggle-menu');
-const closeMenu = document.getElementById('navbar__close-menu');
-
 
 toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('navbar__show');
-    console.log("t");
 });
 
-closeMenu.addEventListener('click', () => {
-    navMenu.classList.remove('navbar__show');
-    console.log("c");
+navMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('navbar__show');
 });
